eab2909ea5b49807efb9a263e7e514f5a7449fbb
Apole-dev <Apole.dev@gmail.com>
2025-02-27 21:47:07 +0300

    New functions decleration and error fix.

--- lib/LoRa/LoRa_config.h
+++ lib/LoRa/LoRa_config.h
@@ -11,7 +11,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-
+#include "LoRa_enums.h"
 
 const uint8_t LORA_READ_PARAM   = 0xC1 ;
 const uint8_t LORA_READ_VERSION = 0xC3;
@@ -24,75 +24,38 @@ typedef enum{
     SLEEP
 }LoRa_Mode;
 
-typedef enum {
-    SAVE_PARAM = 0xC0,
-    
-    DO_NOT_SAVE_PARAM = 0xC2,
+typedef struct {
 
+    //PINOUTS
+    uint8_t M0_PIN;
+    uint8_t M1_PIN;
+    uint8_t AUX_PIN;
+    uint8_t RX_PIN;
+    uint8_t TX_PIN;
 
-    PAIRITY_NONE = 0,   // 0-0 
-    PAIRITY_ODD  = 1,   // 0-1
-    PAIRITY_EVEN = 3,   // 1-1
-    
-    BAURD_RATE_9600   = 3,  // 0-1-1 
-    BAURD_RATE_115200 = 7,  // 1-1-1 
-    
-    AIR_DATA_RATE_LONG_RANGE   = 0, // 0-0-0 | 0.3k
-    AIR_DATA_RATE_MEDIUM_RANGE = 2, // 0-1-0 | 2.4k Default
-    AIR_DATA_RATE_SMALL_RANGE  = 5, // 1-0-1 | 19.2K
+    //COMMAND SECTION
 
-    WIRLESS_WAKE_UP_FAST   = 0, // 0-0-0
-    WIRLESS_WAKE_UP_MEDIUM = 4, // 1-0-0 
-    WIRLESS_WAKE_UP_SLOW   = 7, // 1-1-1
+    bool saveParameters;
+    uint16_t address;
 
-    TRANSMISSION_POWER_HIGH    = 0, // 0-0
-    TRANSMISSION_POWER_27dBm   = 1, // 0-1
-    TRANSMISSION_POWER_24dBm   = 2, // 1-0
-    TRANSMISSION_POWER_LOW     = 3, // 1-1
+    enum PARITY_STATE pairty;
+    enum BAUD_RATE baudRate;
+    enum AIR_DATA_RATE airDataRate;
+    uint8_t channelFrequency;
+    bool fixedTransmissionEnable;
+    bool IODriveModeEnable;
+    enum WIRLESS_WAKE_TIME wirlessWakeUpTime;
+    bool FECEnable;
+    enum TRANSMISSION_POWER transmisionPower;
 
-
-}LoRa_Commands ;
-
-typedef struct{
-
-    LoRa_Commands paramState;   //HEAD 8bit 
-    
-    uint8_t HIGH_ADRESS_CODE;   //ADDH 8bit 
-    uint8_t LOW_ADRESS_CODE;    //ADDL 8bit 
-
-    //bool brodcastModeEnable;  //IF it is true : xFFFF HIGH_ADRESS_CODE & LOW_ADRESS_CODE
-
-    LoRa_Commands pairityState; //SPED bit 7-6
-    LoRa_Commands baudRate;     //SPED bit 5-4-3
-    LoRa_Commands airDataRate;  //SPED bit 2-1-0
-
-    //00H-1FH, correspond to 410~441MHz DEFAULT 17H 433MHz
-    uint16_t channelFrequency;       //CHANNEL bit 0-1-2-3-4 in use; 5-6-7 reserved 
-
-    bool fixedTransmissionEnable;    //OPTION   bit 7
-    bool ioDriveModeEnable;                //OPTION   bit 6 
-    LoRa_Commands wirlessWakeUpTime; //OPTION   bit 5-4-3 
-    bool enableFEC;                  //OPTION   bit 2 
-    LoRa_Commands transmissionPower; //OPTION   bit 1-0
-
-
-}LoRa_Command_Setup;
-
-typedef struct{
-
-    uint8_t M0;
-    uint8_t M1;
-    uint32_t GPIO_PORT;    //PORT FOR M0 & M1
-    uint32_t USART_PIN;    //COMMUNUCATION USART_PIN 
-    
-}LoRa_Configs;
+ 
+}LoraConfig;
 
 void LoRa_INIT();
-void LoRa_SendCommand(LoRa_Command_Setup*);
-void LoRa_ReciveResponse(LoRa_Configs*,uint8_t*);
-void LoRa_ModeSet(LoRa_Mode*,LoRa_Configs*);
-bool LoRa_ReadOperatingParams(LoRa_Mode* ,LoRa_Command_Setup*,LoRa_Configs*,uint8_t*);
-bool LoRa_ReadVersionNumber(LoRa_Mode*, LoRa_Command_Setup*,LoRa_Configs*,uint8_t*);
-bool LoRa_Reset(LoRa_Mode*,LoRa_Command_Setup*,LoRa_Configs*,uint8_t*);
+void LoRa_SendCommand();
+void LoRa_ModeSet(LoraConfig*,LoRa_Mode);
+bool LoRa_ReadOperatingParams(uint32_t* response);
+bool LoRa_ReadVersionNumber(uint32_t* response);
+bool LoRa_Reset(LoraConfig*);
 
 #endif // !LORA_CONFIG
--- lib/LoRa/LoRa_enums.h
+++ lib/LoRa/LoRa_enums.h
@@ -3,38 +3,49 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-typedef struct {
 
-    //PINOUTS
-    uint8_t M0_PIN;
-    uint8_t M1_PIN;
-    uint8_t AUX_PIN;
-    uint8_t RX_PIN;
-    uint8_t TX_PIN;
+enum PARITY_STATE {
+    PARITY_8N1 = 0b00,  // 8N1 (No parity)
+    PARITY_8O1 = 0b01,  // 8O1 (Odd parity)
+    PARITY_8E1 = 0b10   // 8E1 (Even parity)
+};
 
-    //MODE
-    
+enum BAUD_RATE {
+    BAUD_1200   = 0b000,
+    BAUD_2400   = 0b001,
+    BAUD_4800   = 0b010,
+    BAUD_9600   = 0b011,
+    BAUD_19200  = 0b100,
+    BAUD_38400  = 0b101,
+    BAUD_57600  = 0b110,
+    BAUD_115200 = 0b111
+};
 
-    //COMMAND SECTION
+enum AIR_DATA_RATE {
+    AIR_RATE_0_3K = 0b000,  // 0.3 kbps
+    AIR_RATE_1_2K = 0b001,  // 1.2 kbps
+    AIR_RATE_2_4K = 0b010,  // 2.4 kbps
+    AIR_RATE_4_8K = 0b011,  // 4.8 kbps
+    AIR_RATE_9_6K = 0b100,  // 9.6 kbps
+    AIR_RATE_19_2K = 0b101  // 19.2 kbps
+};
 
-    bool saveParameters;
-    uint16_t address;
+enum WIRLESS_WAKE_TIME {
+    WAKE_250MS  = 0b000,  // 250 ms
+    WAKE_500MS  = 0b001,  // 500 ms
+    WAKE_750MS  = 0b010,  // 750 ms
+    WAKE_1000MS = 0b011,  // 1000 ms
+    WAKE_1250MS = 0b100,  // 1250 ms
+    WAKE_1500MS = 0b101,  // 1500 ms
+    WAKE_1750MS = 0b110,  // 1750 ms
+    WAKE_2000MS = 0b111   // 2000 ms
+};
 
-    /*
-    PARITY_STATE pairty;
-    BAUD_RATE baudRate;
-    AIR_DATA_RATE airDataRate;
-    uint8_t channelFrequency;
-    bool fixedTransmissionEnable;
-    bool IODriveModeEnable;
-    WIRLESS_WAKE_TIME wirlessWakeUpTime;
-    bool FECEnable;
-    TRANSMISSION_POWER transmisionPower;
-    */
-
- 
-}LoraConfig;
-
-
+enum TRANSMISSION_POWER {
+    POWER_30DBM = 0b00,  // 30 dBm
+    POWER_27DBM = 0b01,  // 27 dBm
+    POWER_24DBM = 0b10,  // 24 dBm
+    POWER_21DBM = 0b11   // 21 dBm
+};
 
 #endif // !LORA_ENUMS
